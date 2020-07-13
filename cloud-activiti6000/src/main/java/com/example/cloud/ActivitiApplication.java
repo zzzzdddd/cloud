31fad7dabcb97196edfe7301dab6d4466a05230f
赵丁 <18435185303@163.com>
2020-07-13 22:44:00 +0800

    创建nacos

---
+++ cloud-activiti6000/src/main/java/com/example/cloud/ActivitiApplication.java
@@ -0,0 +1,11 @@
+package com.example.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiApplication.class,args);
+    }
+}
---
+++ cloud-activiti6000/src/main/java/com/example/cloud/config/ActivitiConfig.java
@@ -0,0 +1,38 @@
+package com.example.cloud.config;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+@Configuration
+public class ActivitiConfig {
+
+    @Resource
+    private DataSource dataSource;
+
+    /**
+     * 初始化配置，将创建25张表
+     *
+     * @return
+     */
+
+    @Bean
+    public StandaloneProcessEngineConfiguration processEngineConfiguration() {
+        StandaloneProcessEngineConfiguration configuration = new StandaloneProcessEngineConfiguration();
+        configuration.setDataSource(dataSource);
+        configuration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+        configuration.setAsyncExecutorActivate(false);
+        return configuration;
+    }
+
+    @Bean
+    public ProcessEngine processEngine() {
+        return processEngineConfiguration().buildProcessEngine();
+    }
+
+}
